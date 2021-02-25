74c5cbaccad77268d96365f013cf5d5f625de01c
qaqc123 <64720974+qaqc123@users.noreply.github.com>
2021-02-25 17:56:05 +0800

    Update jd_factory.js

--- jd_factory.js
+++ jd_factory.js
@@ -52,7 +52,7 @@ if ($.isNode()) {
 }
 let wantProduct = ``;//心仪商品名称
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-const inviteCodes = [];
+const inviteCodes = ['T0225KkcRBgf8QaBKRP0nP8LJgCjVWnYaS5kRrbA'];
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {
